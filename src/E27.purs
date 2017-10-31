27da0fa4897054835957af8045061b4b1d1d6e88
Denys Galtsev <galtsevdv@gmail.com>
2017-10-30 22:19:14 -0700

    e27 working

--- src/E27.purs
+++ src/E27.purs
@@ -2,21 +2,15 @@ module E27 where
 
 import Prelude
 -- import Data.List ((..),filter,length)
-import Data.BigInt (fromInt)
+import Data.BigInt (BigInt,fromInt, prime)
 
 -- import Euler (prime,foldRange)
 
 target:: Int
 target = 1000
 
-primSeqLen:: Int -> Int -> Int
-primSeqLen a b = psl 0
-    where
-        psl x | prime (x*x+a*x+b) = psl (x+1)
-        psl x = x
-
-primSeqLen':: Int -> Int -> BigInt
-primSeqLen' ia ib = let
+primSeqLen:: Int -> Int -> BigInt
+primSeqLen ia ib = let
         a = fromInt ia
         b = fromInt ib
         n1 = fromInt 1
@@ -30,24 +24,18 @@ foldR f acc start end = fr acc start
         fr a cur | cur>end = a
         fr a cur = fr (f a cur) (cur+1)
 
-e27:: Unit -> Int
-e27 unit = foldR foldForA {mab:0, seqLen:0} (1-target) (target-1) # \{mab:x}->x
-    where
-        foldForA acc a = foldR (foldForB a) acc (-target) target
-        foldForB a acc b = let
-                slen = primSeqLen a b
-            in if slen > acc.seqLen then {mab:a*b,seqLen:slen} else acc
-
 type Res = {a::Int, b::Int, seq::BigInt}
 
 showRes:: Res -> String
 showRes r = "Res{a:" <> show r.a <> ", b:" <> show r.b <> ", seq:" <> show r.seq <> "}"
 
-e27':: Unit -> String
-e27' unit = foldR foldB {a:0, b:0, seq:fromInt 0} (-target) target # showRes
+e27:: Unit -> String
+e27 unit = foldR foldB {a:0, b:0, seq:fromInt 0} (-target) target # showRes
     where
-        foldB acc b | not (prime b) = acc
+        foldB:: Res -> Int -> Res
+        foldB acc b | not (prime (fromInt b)) = acc
         foldB acc b = foldR (foldA b) acc (1-target) (target-1)
+        foldA:: Int -> Res -> Int -> Res
         foldA b acc a = let
                 slen = primSeqLen a b
             in if slen > acc.seq then {a:a, b:b, seq:slen} else acc
--- test/Test/E27.purs
+++ test/Test/E27.purs
@@ -6,14 +6,16 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Test.Assert (ASSERT, assert)
 
+import Data.BigInt (fromInt)
+
 import E27 (primSeqLen, foldR)
 
 testMe:: forall e. Eff (assert :: ASSERT, console :: CONSOLE | e) Unit
 testMe = do
     log "start E27"
 
-    assert $ primSeqLen 1 41 == 40
-    assert $ primSeqLen (-79)  1601 == 80
+    assert $ primSeqLen 1 41 == fromInt 40
+    assert $ primSeqLen (-79)  1601 == fromInt 80
 
     assert $ foldR (+) 3 3 7 == 3+3+4+5+6+7
     assert $ foldR (*) 11 2 5 == 11*2*3*4*5
