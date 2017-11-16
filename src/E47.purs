c08d2dc01610ab115baba800db33ca4fc0ba7106
Denis Galtsev <galtsevdv@gmail.com>
2017-11-16 20:24:40 +0200

    e47

--- src/E47.purs
+++ src/E47.purs
@@ -1,7 +1,8 @@
 module E47 where
 
 import Prelude
-import Data.List (List(..),(:))
+import Data.List (List(..),(:),range,length,concat,sort)
+import Data.Foldable (all)
 import Euler (prime)
 
 fct:: Int -> Int -> List Int -> List Int
@@ -17,3 +18,23 @@ fct v d acc = fct v (d+1) acc
 
 factors:: Int -> List Int
 factors a = fct a 2 Nil
+
+distinct:: List Int -> Boolean
+distinct Nil = true
+distinct (x:Nil) = true
+distinct (a:b:Nil) | a==b = false
+distinct (x:xs) = distinct xs
+
+correct:: Int -> Int -> Boolean
+correct n v = let
+        ll:: List (List Int)
+        ll = range v (v+n-1) # map factors
+    in all (\xs->length xs==n) ll && (concat ll # sort # distinct)
+
+ee:: Int -> Int
+ee n | correct 4 n = n
+ee n = ee (n+1)
+
+
+e47:: Unit -> Int
+e47 unit = ee 2
--- test/Test/E47.purs
+++ test/Test/E47.purs
@@ -2,10 +2,14 @@ module Test.E47 where
 
 import Prelude
 import Test.Testing (TestMe,log,assert)
-import Data.List (List(..),(:))
-import E47 (factors)
+import Data.List (List(..),(:),(..))
+import Data.Foldable (all)
+import E47 (factors,correct)
 
 testMe:: forall e. TestMe e
 testMe = do
     log "E47"
     assert $ map factors [3,6,8,12,16*9, 98] == [3:Nil, 3:2:Nil, 8:Nil, 3:4:Nil, 9:16:Nil, 49:2:Nil]
+    assert $ correct 2 14
+    assert $ all (correct 3 >>> not) (600..643)
+    assert $ correct 3 644
